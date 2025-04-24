5d1b05e84ce4052f9707b0e6210d175ad0a8e1df
JoaoMatheus <jmdelins1@outlook.com>
2025-04-24 16:19:10 -0300

    def __str__  adicionado

--- django-service/api/core/models.py
+++ django-service/api/core/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class User(models.model):
     name = models.CharField(max_length=500)
     email = models.EmailField(max_length=500)
+
+
+def __str__(self):
+    return self.name
